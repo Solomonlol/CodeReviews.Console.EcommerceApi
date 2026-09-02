3fb2b3f7d7864b465af11fd052bc86ff8489b442
Arsenii Hapechkin <Solomonlol95@gmail.com>
2026-09-02 18:08:57 +0300

    start auth

--- Solomonlol.EcommerseApi/Program.cs
+++ Solomonlol.EcommerseApi/Program.cs
@@ -1,19 +1,41 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 using Solomonlol.EcommerseApi;
+using Solomonlol.EcommerseApi.Auth;
 using Solomonlol.EcommerseApi.Endpoints;
 using Solomonlol.EcommerseApi.Interfaces;
 using Solomonlol.EcommerseApi.Mapping;
 using Solomonlol.EcommerseApi.Models.Base;
 using Solomonlol.EcommerseApi.Seeding;
 using Solomonlol.EcommerseApi.Services;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("MSSQLServer");
 
 builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience= true,
+            ValidAudience=AuthOptions.AUDIENCE,
+            ValidateLifetime=true,
+            IssuerSigningKey=AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true
+        };
+    });
 
 builder.Services.AddDbContext<ApplicationContext>(options =>
                             options.UseSqlServer(connectionString));
@@ -39,6 +61,9 @@ builder.Services.AddScoped<IAttributeValueService, AttributeService>();
 builder.Services.AddScoped<ISaleService, SaleService>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 app.UseSwagger();
